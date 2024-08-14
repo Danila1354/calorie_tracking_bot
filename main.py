283753d28927c19c7e5cb1e3c2125ce4b314ff75
Pachev_Danila <pachev-danila@mail.ru>
2024-08-14 11:03:46 +0300

    Fix user directory creation

--- main.py
+++ main.py
@@ -4,17 +4,11 @@ from bot.config import TOKEN
 import os
 from bot.handlers import register_all_handlers
 
-
-if not os.path.exists('data/users'):
-    os.mkdir('data/users')
+os.makedirs('data/users', exist_ok=True)
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot,storage = MemoryStorage())
+dp = Dispatcher(bot, storage=MemoryStorage())
 # register all handlers
 register_all_handlers(dp)
 
-
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-
-
-
